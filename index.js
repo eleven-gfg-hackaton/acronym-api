8aea2ec7e55b1ebeddf2f68dd51769cf7e6c17b9
Hoang Nguyen <148033084+sonhoang1997@users.noreply.github.com>
2024-03-06 22:26:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const functions = require('@google-cloud/functions-framework');
-const {VertexAI, HarmCategory, HarmBlockThreshold, GoogleSearchRetrievalTool, RetrievalTool} = require('@google-cloud/vertexai');
+const {VertexAI} = require('@google-cloud/vertexai');
 
 const project = 'gfg-hackathon2-team-11';
 const location = 'us-central1';
 // For the latest list of available Gemini models in Vertex, please refer to https://cloud.google.com/vertex-ai/docs/generative-ai/learn/models#gemini-models
-const textModel =  'gemini-1.0-pro';
+const visionModel = 'gemini-1.0-pro-vision';
 
 const vertex_ai = new VertexAI({project: project, location: location});
 
-// Instantiate models
-const generativeModel = vertex_ai.getGenerativeModel({
-    model: textModel,
-    // The following parameters are optional
-    // They can also be passed to individual content generation requests
-    safety_settings: [{category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE}],
-    generation_config: {max_output_tokens: 256},
-  });
-
+const generativeVisionModel = vertex_ai.getGenerativeModel({
+    model: visionModel,
+});
 async function extractAcryonyms(inputText) {
     const task = "Extract acronyms from this passage and return an array: " + inputText; 
     const request = {
       contents: [{role: 'user', parts: [{text: task}]}],
     };
-    const streamingResp = await generativeModel.generateContentStream(request);
+    const streamingResp = await generativeVisionModel.generateContentStream(request);
     for await (const item of streamingResp.stream) {
       console.log('stream chunk: ', JSON.stringify(item));
     }
@@ -37,11 +31,22 @@ async function extractAcryonyms(inputText) {
 functions.http('helloGET', async (req, res) => {
     try {
         const inputText = req.body.text;
-        console.log(inputText);
         const content = await extractAcryonyms(inputText);
-        res.status(200).send(content);
+        let acronyms = JSON.parse(content);
+        acronyms = acronyms.map(acronym => {
+            return {
+                acronym: acronym,
+                abbrFor: [
+                    {
+                      name: 'Acknowledgement',
+                      meaning: 'Acknowledgement'
+                    },
+                  ]
+            }
+        })
+        res.status(200).send(acronyms);        
     } catch (error) {
         console.log(error);
-        res.status(500).send(error.message)
+        res.status(200).send([]);        
     }
   });
