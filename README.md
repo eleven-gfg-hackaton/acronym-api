d6445ebe189917ab2a8b267b950f261ba8308b84
Hoang Nguyen <148033084+sonhoang1997@users.noreply.github.com>
2024-03-06 21:42:01 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # acronym-api
 
- gcloud auth application-default login
+To deploy the `nodejs-http-function` using `gcloud`, run the following command:
+gcloud functions deploy nodejs-http-function \
+--gen2 \
+--runtime=nodejs20 \
+--region=us-central1 \
+--source=. \
+--entry-point=helloGET \
+--trigger-http 
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const app = express();
+const functions = require('@google-cloud/functions-framework');
 const {VertexAI, HarmCategory, HarmBlockThreshold, GoogleSearchRetrievalTool, RetrievalTool} = require('@google-cloud/vertexai');
 
 const project = 'gfg-hackathon2-team-11';
 const location = 'us-central1';
 // For the latest list of available Gemini models in Vertex, please refer to https://cloud.google.com/vertex-ai/docs/generative-ai/learn/models#gemini-models
 const textModel =  'gemini-1.0-pro';
-const visionModel = 'gemini-1.0-pro-vision';
 
 const vertex_ai = new VertexAI({project: project, location: location});
 
@@ -19,12 +17,8 @@ const generativeModel = vertex_ai.getGenerativeModel({
     generation_config: {max_output_tokens: 256},
   });
 
-const generativeVisionModel = vertex_ai.getGenerativeModel({
-    model: visionModel,
-});
-
 async function extractAcryonyms(inputText) {
-    const task = "Extract acronyms from this passage: " + inputText; 
+    const task = "Extract acronyms from this passage and return an array: " + inputText; 
     const request = {
       contents: [{role: 'user', parts: [{text: task}]}],
     };
@@ -40,10 +34,7 @@ async function extractAcryonyms(inputText) {
     return fullTextResponse;
 }
 
-app.use(express.json());
-
-// Add your routes here
-app.post('/', async (req, res) => {
+functions.http('helloGET', async (req, res) => {
     try {
         const inputText = req.body.text;
         console.log(inputText);
@@ -53,10 +44,4 @@ app.post('/', async (req, res) => {
         console.log(error);
         res.status(500).send(error.message)
     }
-});
-
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+  });
