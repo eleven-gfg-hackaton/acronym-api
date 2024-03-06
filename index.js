4a6d4f246c161ccc1195fd4a3eb26e0ecefd4053
Hoang Nguyen <148033084+sonhoang1997@users.noreply.github.com>
2024-03-06 23:57:21 +0700

    update

--- index.js
+++ index.js
@@ -11,16 +11,12 @@ const vertex_ai = new VertexAI({project: project, location: location});
 const generativeVisionModel = vertex_ai.getGenerativeModel({
     model: visionModel,
 });
-async function extractAcryonyms(inputText) {
-    const task = "Extract acronyms from this passage and return an array: " + inputText; 
-    const request = {
-      contents: [{role: 'user', parts: [{text: task}]}],
-    };
+
+async function generateContentStream(request) {
     const streamingResp = await generativeVisionModel.generateContentStream(request);
     for await (const item of streamingResp.stream) {
       console.log('stream chunk: ', JSON.stringify(item));
     }
-    console.log('aggregated response: ', JSON.stringify(await streamingResp.response));
     const aggregatedResponse = await streamingResp.response;
     const fullTextResponse =
     aggregatedResponse.candidates[0].content.parts[0].text;
@@ -28,22 +24,59 @@ async function extractAcryonyms(inputText) {
     return fullTextResponse;
 }
 
+async function extractAcryonyms(inputText) {
+    const task = `Extract the acronyms from the text below in an array format:
+    TEXT: ${inputText}`; 
+    const request = {
+      contents: [{role: 'user', parts: [{text: task}]}],
+    };
+    
+    return await generateContentStream(request);
+}
+
+
+async function getNameOfAcronym(acronym) {
+    const task = `What is ${acronym} stands for?`; 
+    const request = {
+      contents: [{role: 'user', parts: [{text: task}]}],
+    };
+    return await generateContentStream(request);
+}
+
+async function getMeaningOfName(name) {
+    const task = `What is ${name}?`; 
+    const request = {
+      contents: [{role: 'user', parts: [{text: task}]}],
+    };
+    return await generateContentStream(request);
+}
+
+
 functions.http('helloGET', async (req, res) => {
     try {
         const inputText = req.body.text;
         const content = await extractAcryonyms(inputText);
-        let acronyms = JSON.parse(content);
-        acronyms = acronyms.map(acronym => {
+        let acronyms = [];
+        try {
+            acronyms = JSON.parse(content);
+        } catch (error) {
+            acronyms = content.split('\n').map(item => item.trim().substring(2));
+        }
+
+        acronyms = await Promise.all(acronyms.map( async acronym => {
+            let name = await getNameOfAcronym(acronym);
+            let meaning = await getMeaningOfName(name);
             return {
                 acronym: acronym,
                 abbrFor: [
                     {
-                      name: 'Acknowledgement',
-                      meaning: 'Acknowledgement'
+                      name: name,
+                      meaning: meaning
                     },
                   ]
             }
-        })
+        }));
+
         res.status(200).send(acronyms);        
     } catch (error) {
         console.log(error);
